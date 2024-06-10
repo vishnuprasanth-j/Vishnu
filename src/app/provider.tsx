56ab6c8ced000c312c88a51ffffab6495f9ac655
vishnuprasanth-j <jvpvis6@gmail.com>
2024-06-10 23:14:13 +0530

    fix type

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,9 +1,19 @@
 'use-client';
-import '../styles/globals.css';
-import * as React from 'react';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import type { ThemeProviderProps } from 'next-themes/dist/types';
+import * as React from 'react';
 
-export function Providers({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider attribute='class' defaultTheme='system' enableSystem>{children}</NextThemesProvider>;
+import '../styles/globals.css';
+
+export function Providers({
+  children,
+  attribute,
+  defaultTheme,
+  enableSystem,
+}: ThemeProviderProps) {
+  return (
+    <NextThemesProvider attribute='class' defaultTheme='system' enableSystem>
+      {children}
+    </NextThemesProvider>
+  );
 }
