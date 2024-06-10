21500987ec45272a87bdbb8572cc1a71807fbec5
vishnuprasanth-j <jvpvis6@gmail.com>
2024-06-10 22:51:29 +0530

    fix eslint

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,20 +1,11 @@
-
 'use client';
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
 import { ThemeProvider } from 'next-themes';
-
 import '../styles/globals.css';
 
 export const Providers: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
   return (
     <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
       {children}
     </ThemeProvider>
   );
-}
-
-
-
+};
